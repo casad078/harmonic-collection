83d3d098d96a10ee80cd87e2f789f78733b7efaf
Dani C <143529026+casad078@users.noreply.github.com>
2023-10-24 11:14:08 -0400

    update

--- entry-6a/index.js
+++ entry-6a/index.js
@@ -1,6 +1,6 @@
 function checkPassword() {
   var password = document.getElementById("password").value;
-  if (password === "smutheredinthecrib") {
+  if (password === "motherlylove") {
     window.location.href = "../";
     return false;
   } else {
