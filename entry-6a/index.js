e132d08943ff18df7a261516e5a36534e608c87d
Dani C <143529026+casad078@users.noreply.github.com>
2023-10-30 21:16:41 -0400

    entry-7

--- entry-6a/index.js
+++ entry-6a/index.js
@@ -1,7 +1,7 @@
 function checkPassword() {
   var password = document.getElementById("password").value;
   if (password === "motherlylove") {
-    window.location.href = "../";
+    window.location.href = "../entry-7";
     return false;
   } else {
     alert("Incorrect password. Please try again.");
