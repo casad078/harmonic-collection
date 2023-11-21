6d5b900f0f6166df1a26f364da6b084fad774dcb
Dani C <143529026+casad078@users.noreply.github.com>
2023-11-20 23:04:36 -0500

    update

--- entry-10/index.js
+++ entry-10/index.js
@@ -71,3 +71,4 @@ class Typerwriter {
       repeat: true,
     })
   })
+
--- entry-3/index.js
+++ entry-3/index.js
@@ -18,3 +18,16 @@ function topFunction() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
 }
+
+
+/*form*/
+function checkPassword() {
+  var password = document.getElementById("password").value;
+  if (password === "74386") {
+    window.location.href = "https://casad078.github.io/harmonic-collection/entry-5/";
+    return false;
+  } else {
+    alert("Incorrect password. Please try again.");
+    return false;
+  }
+}
