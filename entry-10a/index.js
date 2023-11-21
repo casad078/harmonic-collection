0bec2c5e580b858367495056b00f5f4e2205a938
Dani C <143529026+casad078@users.noreply.github.com>
2023-11-20 20:14:36 -0500

    entry-10

---
+++ entry-10a/index.js
@@ -0,0 +1,11 @@
+    // Change the URL to the desired destination
+    var destinationUrl = 'https://www.pinterest.com/phaliba22/pins/';
+
+    // Set the delay in milliseconds (e.g., 5000 milliseconds for 5 seconds)
+    var delay = 1000;
+
+    // Use setTimeout to wait for the specified delay before redirecting
+    setTimeout(function() {
+      // Redirect to the destination URL
+      window.location.href = destinationUrl;
+    }, delay);
