81a1b90b4cdb27f4c2797b0f1433379612bba784
Dani C <143529026+casad078@users.noreply.github.com>
2023-11-27 22:17:39 -0500

    entry-11

---
+++ entry-11/sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(720, 400);
+}
+
+function draw() {
+  background(102);
+
+  push();
+  translate(width * 0.2, height * 0.5);
+  rotate(300.0);
+  star(0, 0, 20, 70, 6);
+  pop();
+  
+}
+
+function star(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
