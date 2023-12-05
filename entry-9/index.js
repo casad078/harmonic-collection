d7cd0be0fbe74b4711720777e41ffdf1e5c48300
Dani C <143529026+casad078@users.noreply.github.com>
2023-12-05 02:53:22 -0500

    good enough

--- entry-9/index.js
+++ entry-9/index.js
@@ -75,3 +75,14 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+/*mobile nav*/
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
